109e2e403745f61c76ae81f3ae2d0b4cf73cb61a
nelyne1014 <nelyne1014@gmail.com>
2019-05-21 17:49:29 +0300

    נלי

--- Ex03.GarageLogic/FuelTank.cs
+++ Ex03.GarageLogic/FuelTank.cs
@@ -8,17 +8,24 @@ namespace Ex03.GarageLogic
     public class FuelTank : Engine
     {
         private float m_CurrentFuelCapacity;
-        private float m_MaxFuelCapacity;
+        private readonly float m_MaxFuelCapacity;
         private eFuelType m_FuelType;
 
-        public FuelTank(eFuelType i_FuelType,float i_MaxFuelCapacity)
+        public FuelTank(eFuelType i_FuelType, float i_MaxFuelCapacity)
         {
             m_FuelType = i_FuelType;
             m_MaxFuelCapacity = i_MaxFuelCapacity;
         }
         public override void FillEnergy(float i_FuelLiterToAdd) //, eFuelType i_FuelType)
         {
-
+            if (m_CurrentFuelCapacity + i_FuelLiterToAdd <= m_MaxFuelCapacity)
+            {
+                m_CurrentFuelCapacity += i_FuelLiterToAdd;
+            }
+            else
+            {
+                //Exceptions
+            };
         }
     }
 }
