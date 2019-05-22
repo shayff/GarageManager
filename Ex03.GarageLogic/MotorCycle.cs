b7af823c8181f95fc5946665ef3de159af120e01
shayff <shayff@users.noreply.github.com>
2019-05-22 17:43:49 +0300

    Energy Source update

--- Ex03.GarageLogic/MotorCycle.cs
+++ Ex03.GarageLogic/MotorCycle.cs
@@ -16,7 +16,7 @@ namespace Ex03.GarageLogic
         //*Methods*//
         public MotorCycle(eEnergyType i_EnergySource, string i_NameOfModel, string i_LicenseNumber) :base(i_EnergySource, i_NameOfModel, i_LicenseNumber, k_NumberOfWheels, k_MaxAirPressure)
         {
-
+            
         }
     }
 }
