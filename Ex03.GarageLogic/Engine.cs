3f4d8dc47e130d51a891097b9abae44fd3ac79f4
shayff <shayff@users.noreply.github.com>
2019-05-22 18:16:17 +0300

    Updated Engine class

--- Ex03.GarageLogic/Engine.cs
+++ Ex03.GarageLogic/Engine.cs
@@ -10,17 +10,29 @@ namespace Ex03.GarageLogic
 
     public class Engine
     {
+        const string k_ErrorTooMuchFuel = "You try to add to much fuel";
+        const string k_ErrorWrongTypeOfFuel = "You try to add wrong type of fuel";
+
         private eFuelType m_FuelType;
+        private float m_CurrentFuelCapacity;
+        private readonly float r_MaxFuelCapacity;
 
         public  void FillEnergy(eFuelType i_FuelTypeToAdd , float i_AmountToAdd)
         {
             if(i_FuelTypeToAdd == m_FuelType)
             {
-
+                if (i_AmountToAdd + m_CurrentFuelCapacity <= r_MaxFuelCapacity)
+                {
+                    m_CurrentFuelCapacity += i_AmountToAdd;
+                }
+                else
+                {
+                    throw new ValueOutOfRangeException(0, r_MaxFuelCapacity, k_ErrorTooMuchFuel);
+                }
             }
             else
             {
-                //throw not same
+                throw new Exception(k_ErrorWrongTypeOfFuel);
             }
         }
     }
